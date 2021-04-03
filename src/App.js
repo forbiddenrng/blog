7add532503b4335aa9bd1cf689b0585664fe8ee6
Antek <antek.gawron@gmail.com>
2021-04-03 12:18:18 +0200

    basic route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
 import Header from './components/Header'
-function App() {
-  return (
-    <div>
-      <Header />
-      <div>Coś tu jeszcze będzie</div>
-    </div>
-  );
+import About from './components/About'
+import "./App.css"
+// const About = () => <h1>O nas</h1>
+const Travels = () => <h1>Podróże</h1>
+const Add = () => <h1>Dodaj swoją podróż</h1>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <nav>
+            <ul>
+              <li><NavLink to="/">Strona główna</NavLink></li>
+              <li><NavLink to="/travels">Podróże</NavLink></li>
+              <li><NavLink to="/add">Dodaj</NavLink></li>
+            </ul>
+          </nav>
+          <section>
+            <Switch>
+              <Route path="/" exact>
+                <About />
+              </Route>
+              <Route path="/travels">
+                <Travels />
+              </Route>
+              <Route path="/add">
+                <Add />
+              </Route>
+            </Switch>
+          </section>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css'
+const About = (props) => {
+  return (
+    <section>
+      <h2>Witaj na naszym blogu</h2>
+      <p>Jesteśmy 2D, mat geo and coś tu jeszcze trzeba będzie napisać ale nie wiem co</p>
+    </section>
+  )
+}
+export default About;
